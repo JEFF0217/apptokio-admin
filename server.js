90dd47da2436eaeae69c04ce384ea8186e65515d
JEFF0217 <jefferson.1701717549@ucaldas.edu.co>
2021-07-31 10:10:34 -0500

    deploy second try

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const path = require('path');
+
 const express = require('express');
-const app = express();
+const path = require('path');
 
-// Serve static files
-app.use(express.static(__dirname + '/apptokio/'));
+const ngApp = express();
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/apptokio/index.html'));
+ngApp.use(express.static('./dist/angular-forms-validation'));
+
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+ngApp.listen(process.env.PORT || 8080);
